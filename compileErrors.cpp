2dcf957a139febfd5ae2bb48bcae736b05e8fc19
Florin Hasna <florinhasna@outlook.com>
2023-12-08 09:22:27 +0000

    Fixed the bug, program works properly

---
+++ compileErrors.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Please enter two numbers:";
+    int x = 0;
+    int y = 0;
+    cin >> x >> y;
+    cout << "The sum of " << x << " and " << y << " is: " << x + y << std::endl;
+    return 0;
+}
